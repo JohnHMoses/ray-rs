2696aa396a65a6a5cfa0d510886f8137165d347b
John Moses <john.moses445@gmail.com>
2017-08-09 18:21:15 -0700

    Added TransformableElementBuilder parsing tree

--- src/parser/ray_scene_builder/mod.rs
+++ src/parser/ray_scene_builder/mod.rs
@@ -23,27 +23,92 @@ impl SceneBuilder {
 		SceneBuilder {
 			lights: Vec::new(),
 			objects: Vec::new(),
+            root_transform: TransformNode::new(),
 			camera: None,
 			ambient: None,
 		}
 	}
 
 	pub fn parse_scene(&mut self, tokenizer: &mut Tokenizer) {
-		// TODO
+		// TODO version parsing at top
 		//if let Some(Token::SbtRaytracer) = tokenizer.next() {
 		//	if let Some()
 		//}
-		unimplemented!();
+        let token_option = tokenizer.peek();
+        match token_option {
+            Some(token) => match token {
+                Token::Sphere |
+                Token::Box |
+                Token::Square |
+                Token::Cylinder |
+                Token::Cone |
+                Token::Trimesh |
+                Token::Translate |
+                Token::Rotate |
+                Token::Scale |
+                Token::Transform |
+                Token::LBrace => {
+                    objects.add( TransformableObjectBuilder::new(tokenizer, root_transform) );
+                }
+                Token::PointLight => unimplemented!(),
+                Token::DirectionalLight => unimplemented!(),
+                Token::AmbientLight => unimplemented!(),
+                Token::Camera => unimplemented!(),
+                Token::Material => unimplemented!(),
+                _ => unimplemented!(), // Synxtax error, unexpected token
+            },
+            None => unimplemented!(), // EOF
+        }
 	}
 
 	pub fn create_scene(&self) -> Scene {
 		// TODO
 		Scene::new()
 	}
-
 	// TODO: setters
 }
 
+enum TransformableElement {
+    Geometry(GeometryBuilder),
+    Group(GroupBuilder),
+}
+
+struct TransformableElementBuilder {
+    element: TransformableElement,
+}
+
+impl TransformableElementBuilder {
+    pub fn new() -> TransformableElementBuilder {
+        TransformableElementBuilder { element: None }
+    }
+
+    pub fn parse_transformable_element(&mut self, tokenizer: &mut Tokenizer, transform_node: &TransformNode) -> TransformableElementBuilder {
+        let token_option = tokenizer.peek();
+        match token_option {
+            Some(token) => match token {
+                Token::Sphere |
+                Token::Box |
+                Token::Square |
+                Token::Cylinder |
+                Token::Cone |
+                Token::Trimesh |
+                Token::Rotate |
+                Token::Scale |
+                Token::Transform => {
+                    self.element = Geometry(GeometryBuilder::new(tokenizer, transform_node));
+                },
+                Token::LBrace => {
+                    self.element = Group(GroupBuilder::new(tokenizer, transform_node));
+                },
+                Token::Material => unimplemented!(),
+                _ => unimplemented!(), // Syntax error
+            },
+            None => unimplemented!(), // Syntax error
+        }
+
+
+    }
+}
+
 struct LightBuilder;
-struct TransformableObjectBuilder;
 struct CameraBuilder;
